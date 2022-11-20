ad26b6da4bd9f94e396296108f6dc8851c4e23ca
Roman Zharov <zhr.public@gmail.com>
2022-11-21 01:24:36 +0600

    Seminar 01 Task 08 added, Task 01 modified

--- Seminar_01_Task01/Program.cs
+++ Seminar_01_Task01/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
 
 Console.WriteLine("Enter the number: ");
 int num01 = Convert.ToInt32(Console.ReadLine());
---
+++ Seminar_01_Task08/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите целое число в диапазоне от 1 до 1000");
+int numInputInt = Convert.ToInt32(Console.ReadLine());
+
+if (numInputInt >= 1 && numInputInt <= 1000)
+{
+    int numNext = 1;
+    while (numNext <= numInputInt)
+    {
+        if (numNext%2 == 0)
+        {
+            Console.Write($"{numNext} | ");
+            numNext++;
+        }
+        else
+        {
+            numNext++;
+        }
+    }
+}
+else
+{
+    Console.WriteLine($"Вы ввели число вне диапазона");
+}
