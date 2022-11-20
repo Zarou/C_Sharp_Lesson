e895c22e9815406aa195f80e1b6ae33fba7a7346
Roman Zharov <zhr.public@gmail.com>
2022-11-20 22:24:13 +0600

    Seminar 01 Task 05 added

---
+++ Seminar_01_Task05/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Введите целое число в диапазоне от 1 до 100");
+int num = Convert.ToInt32(Console.ReadLine());
+int numOutput = -num;
+
+if (num > 0 && num < 101)
+{
+    while (numOutput <= num)
+    {
+        Console.Write($"{numOutput} | ");
+        numOutput++;
+    }
+} else {
+    Console.WriteLine($"Вы ввели число вне возможного диапазона!");
+}
