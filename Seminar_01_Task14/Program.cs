925053a7d498336dca7897ef8a7f4e4f86231af0
Roman Zharov <zhr.public@gmail.com>
2022-11-28 01:43:12 +0600

    Task 13 added

---
+++ Seminar_01_Task14/Program.cs
@@ -0,0 +1,9 @@
+﻿// Принимает на вход число и проверяет, кратно ли оно одновременно  7 и 23
+Console.WriteLine("Введите целое число для определения его кратности числам 7 и 23:");
+int inputNum = Convert.ToInt32(Console.ReadLine());
+
+if (inputNum%7 == 0 && inputNum%23 == 0) {
+    Console.WriteLine($"Число {inputNum} кратно числам 7 и 23");
+} else {
+    Console.WriteLine($"Число {inputNum} НЕ ЯВЛЯЕТСЯ кратным одновременно числам 7 и 23");
+}
