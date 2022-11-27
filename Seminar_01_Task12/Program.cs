f376968b2d41f81f54b60dd87db090329ba7057f
Roman Zharov <zhr.public@gmail.com>
2022-11-27 22:50:11 +0600

    Task 12 added

---
+++ Seminar_01_Task12/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа принимает 2 числа на вход и выводит является ли  второе число кратным первому.
+// Если нет, программа выводит остаток от деления
+
+Console.WriteLine("Введите первое число");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+if (firstNum % secondNum == 0) {
+    Console.WriteLine($"Второе число ({secondNum}) ЯВЛЯЕТСЯ кратным первому ({firstNum})");
+}
+else {
+    Console.WriteLine($"Второе число ({secondNum}) НЕ ЯВЛЯЕТСЯ кратным первому ({firstNum}). Остаток - {firstNum % secondNum}");
+}
