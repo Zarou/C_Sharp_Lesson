db2c90c87bc28b942fbf25e5156d555ccc4292a9
Roman Zharov <zhr.public@gmail.com>
2022-12-04 00:23:41 +0600

    Task 22 added

--- Seminar_01_Task00/Program.cs
+++ Seminar_01_Task00/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int[,] numArray = new int[4, 2] {{1,1}, {2,4}, {3,9}, {4,16}};
+OutputArray(numArray);
+
+void OutputArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("Квадрат числа {0} = {1}\n", array[i, 0], array[i, 1]);
+    }
+}
---
+++ Seminar_01_Task22/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите целое число:");
+int inputNum = Convert.ToInt32(Console.ReadLine());
+int[,] numbersArray = SquareNumbers(inputNum);
+OutputArray(numbersArray);
+
+int[,] SquareNumbers(int number)
+{
+    int[,] numSquaredArray = new int[number, 2];
+    int n = 1;
+    for (int i = 0; i < number; i++)
+    {
+        numSquaredArray[i, 0] = n;
+        numSquaredArray[i, 1] = n * n;
+        n++;
+    }
+    return numSquaredArray;
+}
+
+void OutputArray(int[,] array)
+{
+    Console.WriteLine("Вычисляем квадраты целых чисел от 1 до {0}:", array.GetLength(0));
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("Квадрат числа {0} = {1}\n", array[i, 0], array[i, 1]);
+    }
+}
