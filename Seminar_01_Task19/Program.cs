853e783b2c7d25e7e6ac3293fdd79b25100db4b2
Roman Zharov <zhr.public@gmail.com>
2022-12-03 12:21:51 +0600

    Task 20 added

--- Seminar_01_Task19/Program.cs
+++ Seminar_01_Task19/Program.cs
@@ -3,7 +3,7 @@
 int inputNum = GetInputNumber();
 int numOfDigits = ((int)Math.Abs(inputNum)).ToString().Length;
 int[] inputNumArray = new int[numOfDigits];
-bool isPalindrom = false;
+bool isPalindrome = false;
 
 int inputNumRec = inputNum;
 for ( int i = (numOfDigits - 1); i >= 0; i--)
@@ -14,10 +14,10 @@ for ( int i = (numOfDigits - 1); i >= 0; i--)
 
 for (int i = 0; i < (inputNumArray.Length / 2); i++)
 {
-    if (inputNumArray[i] == inputNumArray[^(i + 1)]) isPalindrom = true;
+    if (inputNumArray[i] == inputNumArray[^(i + 1)]) isPalindrome = true;
 }
 
-if (isPalindrom) Console.WriteLine($"Число {inputNum} является палиндромом");
+if (isPalindrome) Console.WriteLine($"Число {inputNum} является палиндромом");
 else Console.WriteLine($"Число {inputNum} НЕ является палиндромом");
 
 int GetInputNumber()
---
+++ Seminar_01_Task20/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве
+
+Console.WriteLine("Для определения длины отрезка, введите координаты двух точек в 2D пространстве");
+int pointAcoordX = GetInputNumber('A', 'X');
+int pointAcoordY = GetInputNumber('A', 'Y');
+int pointBcoordX = GetInputNumber('B', 'X');
+int pointBcoordY = GetInputNumber('B', 'Y');
+double lineLength = CalcLineLength(pointAcoordX, pointAcoordY, pointBcoordX, pointBcoordY);
+
+Console.WriteLine($"Длина отрезка A({pointAcoordX}, {pointAcoordY}) - B({pointBcoordX}, {pointBcoordY}) составляет:\n{lineLength}");
+
+int GetInputNumber(char p, char c)
+{
+    Console.WriteLine($"Введите координату {c} точки {p}:");
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+double CalcLineLength(int ax, int ay, int bx, int by)
+{
+    double lengthAB = Math.Round(Math.Sqrt(Math.Pow((bx - ax), 2) + Math.Pow((by - ay), 2)), 2);
+    return lengthAB;
+}
