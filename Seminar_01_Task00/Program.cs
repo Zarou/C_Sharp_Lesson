b987a62c48cf08af5a6dd84397768f488703df99
Roman Zharov <zhr.public@gmail.com>
2022-12-04 03:43:55 +0600

    Task 27 added

--- Seminar_01_Task00/Program.cs
+++ Seminar_01_Task00/Program.cs
@@ -1,10 +1,9 @@
-﻿int[,] numArray = new int[4, 2] {{1,1}, {2,4}, {3,9}, {4,16}};
-OutputArray(numArray);
-
-void OutputArray(int[,] array)
+﻿int number = 54;
+int digitsCount = 1;
+int i = number;
+while (i > 9) 
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        Console.Write("Квадрат числа {0} = {1}\n", array[i, 0], array[i, 1]);
-    }
+    i = i / 10;
+    digitsCount++;
 }
+Console.WriteLine(digitsCount);
---
+++ Seminar_01_Task27/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+Console.WriteLine("Введите число для подсчета суммы цифр в нем:");
+int inputNum = GetInputNum();
+
+Console.WriteLine($"Сумма цифр в числе {inputNum} равна {GetSumOfDigits(inputNum)}");
+
+int GetSumOfDigits(int number)
+{
+    int sum = 0;
+    int digitsCount = 1;
+    int n = number;
+    while (n > 9)
+    {
+        n = n / 10;
+        digitsCount++;
+    }
+    int[] numberArray = new int[digitsCount];
+    int divider = 10;
+    for (int i = (digitsCount - 1); i >= 0; i--)
+    {
+        numberArray[i] = number%divider;
+        number = decimal.ToInt32(number/10);
+    }
+    for (int i = 0; i < numberArray.Length; i++) sum = sum + numberArray[i];
+    return sum;
+}
+
+int GetInputNum()
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
