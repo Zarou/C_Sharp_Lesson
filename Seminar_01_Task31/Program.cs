6cf42c136174c3ef4ca5feb4d97f70001bf370c4
Roman Zharov <zhr.public@gmail.com>
2022-12-06 00:03:01 +0600

    Tasks 29, 31, 32 added

---
+++ Seminar_01_Task31/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+// Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29,
+// сумма отрицательных равна -20. 9,  1, 0,  2,  8, -3, -1, 
+
+int[] numArray = new int[12];
+int sumPos = 0;
+int sumNeg = 0;
+for (int i = 0; i < numArray.Length; i++) numArray[i] = new Random().Next(-9, 10);
+
+foreach (int i in numArray)
+{
+    if (i < 0) sumNeg = sumNeg + i;
+    else sumPos = sumPos + 1;
+}
+
+Console.WriteLine($"В массиве");
+foreach (int i in numArray) Console.Write($"{i} ");
+Console.WriteLine($"\nСумма положительных элементов - {sumPos}\nСумма отрицательных элементов - {sumNeg}");
---
+++ Seminar_01_Task32/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу замены элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] numArray = new int[12];
+int[] numArrayInv = new int[numArray.Length];
+
+for (int i = 0; i < numArray.Length; i++) numArray[i] = new Random().Next(-9, 10);
+for (int i = 0; i < numArray.Length; i++) numArrayInv[i] = numArray[i] * -1;
+
+Console.WriteLine($"Исходный массив:");
+foreach (int i in numArray) Console.Write($"{i}\t");
+Console.WriteLine($"\nИнвертированный массив:");
+foreach (int i in numArrayInv) Console.Write($"{i}\t");
