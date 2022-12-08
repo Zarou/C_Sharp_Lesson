eb09625334bc7d0a471999355a197580d0bf7087
Roman Zharov <zhr.public@gmail.com>
2022-12-09 01:19:50 +0700

    Task 36 added

---
+++ Seminar_01_Task36/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int arrayLength = 100;
+int[] newArray = CreateArray(arrayLength);
+int oddIndexElementSum = newArray[1];
+
+for ( int i = 3; i < newArray.Length; i++)
+{
+    if (i%2 != 0)
+    {
+        oddIndexElementSum = oddIndexElementSum + newArray[i];
+    }
+}
+
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях равна {oddIndexElementSum}");
+
+int[] CreateArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(0, 999);
+    return array;
+}
