565e9799feb96541cb1298230fba0b1349bb6276
Roman Zharov <zhr.public@gmail.com>
2022-12-09 08:57:07 +0700

    Task 37 added

---
+++ Seminar_01_Task37/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// МОЖНО, КОНЕЧНО, ЧЕРЕЗ МЕТОД ГЕНЕРИРОВАТЬ СЛУЧАЙНЫЙ МАСССИВ С ПРОВЕРКОЙ ЧЕТНОСТИ КОЛИЧЕСТВА ЭЛЕМЕНТОВ, НО ЭТО СУТИ НЕ МЕНЯЕТ
+
+int[] newArray = new int[10] {0,1,2,3,4,5,6,7,8,9};
+int[] resultArray = new int[newArray.Length/2];
+
+for (int i = 0; i < newArray.Length/2; i++) resultArray[i] = newArray[i] * newArray[newArray.Length - i - 1];
+foreach (int i in resultArray) Console.Write($"{i}\t");
